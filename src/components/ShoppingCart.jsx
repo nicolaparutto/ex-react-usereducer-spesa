9efd50f9910e3792b7dfdd59135e8a796d8d3d70
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-09 10:15:04 +0200

    feat: milestone 1 completed

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,7 +1,26 @@
+const products = [
+	{ name: 'Mela', price: 0.5 },
+	{ name: 'Pane', price: 1.2 },
+	{ name: 'Latte', price: 1.0 },
+	{ name: 'Pasta', price: 0.7 },
+];
+
 function ShoppingCart() {
 	return (
-		<section>
-			Carrello della spesa
+		<section className="container">
+			<h1>Lista dei prodotti</h1>
+			<div className="products-container">
+				<div className="products-titles">
+					<span>Prodotto</span>
+					<span>Prezzo</span>
+				</div>
+				{products.map((product, index) => (
+					<div className="product">
+						<span>{product.name}</span>
+						<span>{product.price}€</span>
+					</div>
+				))}
+			</div>
 		</section>
 	)
 }
