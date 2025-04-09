84805aa4a750183a4fd30a76ba140ba88c12926c
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-09 11:48:59 +0200

    feat: milestone 2 completed

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -5,23 +5,70 @@ const products = [
 	{ name: 'Pasta', price: 0.7 },
 ];
 
+import { useReducer } from "react";
+
 function ShoppingCart() {
+
+	const [addedProducts, dispatchAddedProducts] = useReducer(reducer, []);
+
+	function reducer(state, action) {
+		switch (action.type) {
+			case "ADD_PRODUCT":
+				if (state.some(product => product.name === action.pName)) {
+					return state;
+				} else {
+					const productToAdd = {
+						name: action.pName,
+						price: action.pPrice,
+						quantity: action.pQuantity
+					}
+					return [...state, productToAdd];
+				}
+
+			default:
+				return state;
+		}
+	}
+
+
 	return (
-		<section className="container">
-			<h1>Lista dei prodotti</h1>
-			<div className="products-container">
-				<div className="products-titles">
-					<span>Prodotto</span>
-					<span>Prezzo</span>
-				</div>
-				{products.map((product, index) => (
-					<div className="product">
-						<span>{product.name}</span>
-						<span>{product.price}€</span>
+		<>
+			<section className="container">
+				<h1>Lista dei prodotti</h1>
+				<div className="products-container">
+					<div className="products-titles">
+						<span>Prodotto</span>
+						<span>Prezzo</span>
 					</div>
-				))}
-			</div>
-		</section>
+					{products.map((product, index) => (
+						<div className="product-item" key={index}>
+							<div className="product-info">
+								<span>{product.name}</span>
+								<span>{product.price}€</span>
+							</div>
+							<div className="add-product-btn" onClick={() => dispatchAddedProducts({ type: "ADD_PRODUCT", pName: product.name, pPrice: product.price, pQuantity: 1 })}>+</div>
+						</div>
+					))}
+				</div>
+			</section>
+
+			<section className="container">
+				<h1>Carrello</h1>
+				<div className="products-container">
+					{
+						addedProducts.length > 0 ?
+							addedProducts.map((product, index) => (
+								<div key={index} className="product-item">
+									<span>{product.name}</span>
+									<span>{product.price}€</span>
+									<span>Quantità: {product.quantity}</span>
+								</div>
+							)) :
+							<div>Il tuo carrello è vuoto!</div>
+					}
+				</div>
+			</section>
+		</>
 	)
 }
 
